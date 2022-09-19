9eabdbc290d5f50260e6a01aabae85e56a3d5842
srushti-Mane <srushtideshmukh2909@gmail.com>
2022-09-19 11:35:03 +0530

    added:welcome to algorithm programs

---
+++ src/com/bridgelabz/PermutationsString.java
@@ -0,0 +1,46 @@
+package com.bridgelabz;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class PermutationsString {
+        public static Set<String> getPermutation(String str) {
+            //  create a set to avoid duplicate permutation
+            Set<String> permutations = new HashSet<String>();
+            // here we  check if string is null
+            if (str == null) {
+                return null;
+
+            } else if (str.length() == 0) {
+                 //terminating condition for recursion
+                permutations.add("");
+                return permutations;
+            }
+             //get the first character
+            char first = str.charAt(0);
+             // get the remaining substring
+            String sub = str.substring(1);
+             //  make recursive call to getPermutation()
+            Set<String> words = getPermutation(sub);
+             // here using for loop access each element from words
+            for (String strNew : words) {
+                for (int i = 0; i <= strNew.length(); i++) {
+                    //insert the permutation to the set
+                    permutations.add(strNew.substring(0, i) + first + strNew.substring(i));
+                }
+            }
+            return permutations;
+        }
+        // create a main method all programs execute in main method
+      // @param args no arguments is default
+    public static void main(String[] args) {
+            //create an object of scanner class
+             //object name is input
+            Scanner input = new Scanner(System.in);
+              //take input from users
+        System.out.print("Enter the string: ");
+            String data = input.nextLine();
+            System.out.println("Permutations of " + data + ": \n" + getPermutation(data));
+        }
+    }
+
